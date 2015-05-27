2e3a6c9687608196f499cfd961580b994c2de705
Steven Jones <steven@layer.com>
2015-05-27 15:07:17 -0700

    setting screen knows user; commented unused bits

--- layer-atlas-messenger/src/main/java/com/layer/atlas/messenger/AtlasSettingsScreen.java
+++ layer-atlas-messenger/src/main/java/com/layer/atlas/messenger/AtlasSettingsScreen.java
@@ -8,18 +8,30 @@ import android.view.View.OnClickListener;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.layer.atlas.Atlas;
+import com.layer.sdk.LayerClient;
+import com.layer.sdk.exceptions.LayerException;
+import com.layer.sdk.listeners.LayerAuthenticationListener;
+import com.layer.sdk.listeners.LayerConnectionListener;
+
+import org.w3c.dom.Text;
+
 /**
  * @author Oleg Orlov
  * @since 17 Apr 2015
  */
-public class AtlasSettingsScreen extends Activity {
+public class AtlasSettingsScreen extends Activity implements LayerAuthenticationListener, LayerConnectionListener {
 
     public static final String EXTRA_FORCE_LOGOUT = "settings.force.logout";
+
+    private App101 app;
+    private TextView usernameTextView;
+    private TextView statusTextView;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        final App101 app = (App101) getApplication();
+        app = (App101) getApplication();
         
         setContentView(R.layout.atlas_screen_settings);
         
@@ -33,8 +45,38 @@ public class AtlasSettingsScreen extends Activity {
                 finish();
             }
         });
-        
+
+        usernameTextView = (TextView)findViewById(R.id.atlas_settings_username_text);
+        statusTextView = (TextView)findViewById(R.id.atlas_settings_login_status_text);
+
         prepareActionBar();
+        updateValues();
+    }
+
+    public void updateValues() {
+        App101 app = (App101)getApplication();
+        LayerClient client = app.getLayerClient();
+        String userId = (client == null)? null : client.getAuthenticatedUserId();
+        Participant participant = (userId == null)? null : app.getParticipantProvider().get(userId);
+
+        usernameTextView.setText(participant == null ? null : Atlas.getFullName(participant)); 
+        statusTextView.setText((client != null && client.isConnected())? "Connected" : "Disconnected");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        app.getLayerClient()
+                .registerAuthenticationListener(this)
+                .registerConnectionListener(this);
+    }
+
+    @Override
+    protected void onPause() {
+        app.getLayerClient()
+                .unregisterAuthenticationListener(this)
+                .unregisterConnectionListener(this);
+        super.onPause();
     }
 
     private void prepareActionBar() {
@@ -46,5 +88,40 @@ public class AtlasSettingsScreen extends Activity {
                 finish();
             }
         });
+    }
+
+    @Override
+    public void onConnectionError(LayerClient layerClient, LayerException e) {
+        updateValues();
+    }
+
+    @Override
+    public void onConnectionDisconnected(LayerClient layerClient) {
+        updateValues();
+    }
+
+    @Override
+    public void onConnectionConnected(LayerClient layerClient) {
+        updateValues();
+    }
+
+    @Override
+    public void onAuthenticated(LayerClient layerClient, String s) {
+        updateValues();
+    }
+
+    @Override
+    public void onDeauthenticated(LayerClient layerClient) {
+        updateValues();
+    }
+
+    @Override
+    public void onAuthenticationChallenge(LayerClient layerClient, String s) {
+        updateValues();
+    }
+
+    @Override
+    public void onAuthenticationError(LayerClient layerClient, LayerException e) {
+        updateValues();
     }
 }
